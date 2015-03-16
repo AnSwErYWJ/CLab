71bc8e82ac2bcac5d7d35d6e399477e650580416
WeiJie Yuan <ywj1993@yeah.net>
2015-03-16 13:30:25 +0800

    四则运算器

--- mathtest.c
+++ mathtest.c
@@ -3,4 +3,48 @@
 	> Author: AnSwEr
 	> Created Time: 2015年03月16日 星期一 12时19分10秒
  ************************************************************************/
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+void swap(int *, int *);
+int main()
+{
+	srand((int)time(0));
+	int i;
+	for(i=0;i<20;i++)
+	{
+		int a=rand()%100;
+		int b=rand()%100;
+		int c=rand()%4;
+		switch(c)
+		{
+			case 0:
+				printf("%d+%d=\n",a,b);
+				break;
+			case 1:
+				printf("%d+%d=\n",a,b);
+				break;
+			case 2:
+				if(a<b)
+					swap(&a,&b);
+				printf("%d-%d=\n",a,b);
+				break;
+			case 3:
+				if(a<b)
+					swap(&a,&b);
+				printf("%d/%d=\n",a,b);
+				break;
+			default:
+				break;
+		}
+	}
+	return 0;
+}
 
+void swap(int *x, int *y)
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
