ad8650cf54e951ed80e3a251a0d3492c7654ecb8
WeiJie Yuan <1045837697@qq.com>
2015-03-18 11:15:37 +0800

    swap two numbers

---
+++ swap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void swap(int *,int *);
+int main()
+{
+int a,b;
+printf("Please enter the numbers:\n");
+printf("a = ");
+scanf("%d",&a);
+printf("b = ");
+scanf("%d",&b);
+swap(&a,&b);
+printf("After swaping:\n");
+printf("a = %d\nb = %d\n",a,b);
+return 0;
+}
+void swap(int * x,int * y)
+{
+int temp;
+temp = *x;
+*x = *y;
+*y = temp;
+}
