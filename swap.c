5e21d0765867496e3e80b7cbe387d2081819d98c
WeiJie Yuan <1045837697@qq.com>
2015-03-18 11:16:45 +0800

    Swap two numbers

--- swap.c
+++ swap.c
@@ -2,21 +2,22 @@
 void swap(int *,int *);
 int main()
 {
-int a,b;
-printf("Please enter the numbers:\n");
-printf("a = ");
-scanf("%d",&a);
-printf("b = ");
-scanf("%d",&b);
-swap(&a,&b);
-printf("After swaping:\n");
-printf("a = %d\nb = %d\n",a,b);
-return 0;
+  int a,b;
+  printf("Please enter the numbers:\n");
+  printf("a = ");
+  scanf("%d",&a);
+  printf("b = ");
+  scanf("%d",&b);
+  swap(&a,&b);
+  printf("After swaping:\n");
+  printf("a = %d\nb = %d\n",a,b);
+  return 0;
 }
+
 void swap(int * x,int * y)
 {
-int temp;
-temp = *x;
-*x = *y;
-*y = temp;
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
