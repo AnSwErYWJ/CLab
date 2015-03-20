6ce12d76c625a12a6adb1bb220cbc1473ec8c021
WeiJie Yuan <1045837697@qq.com>
2015-03-20 12:55:02 +0800

    print a pyramid

---
+++ pyramid.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int rows,i,j;
+    printf("Please enter the rows:");
+    scanf("%d",&rows);
+    for(i=rows; i>=1; i--)
+    {
+        for(j=1; j<=rows-i; j++)
+            printf("  ");
+        for(j=1; j<=2*i-1; j++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
